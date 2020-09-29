3547604d1f211153b70fb3084f708919efe035d0
Ludmila <ludmila_ks@hotmail.com>
2020-09-29 20:50:23 -0300

    adicionando mudancas no pageObject

--- features/steps_definitions/steps_def.js
+++ features/steps_definitions/steps_def.js
@@ -1,20 +1,42 @@
-'use strict';
- 
-const { Given, When, Then, Before } = require('cucumber')
-const { assert, expect, should  } = require('chai');
+const Page = require('../page_objects/base')
+const Login = require('../page_objects/login')
 
-const login = require('../../PageObjects/login')
- 
-// // The step definitions are defined for each of the scenarios // //
+const assert = require('assert')
+const { Given, When, Then, Before, After } = require('cucumber')
+const chai = require('chai')
+const expect = chai.expect
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
 
-  Given('que o usuario acesse a pagina inicial do sistema', function () {
-    login.inicializaDriver()
-    const assert = driver.findElements(webdriver.By.name('form_usuario')).expect(assert).to.be.exist;
-  });
+let driver, page
 
-  // When(/^There is a title on the page as "SeleniumHQ Browser Automation" "([^"]*)"$/, function () {
+Before(async () => {
+    page = new Page()
+    driver = page.driver
+})
 
-  // })
-
-  Then('o campo de Usuario deve ser exibido vazio, para ser preenchido', function () {
+Given('que eu acesse a pagina inicial do sistema', function () {
+    page.visit('http://www.aprendendotestar.com.br/treinar-automacao.php')
   })
+
+Then('o campo de Usuario deve ser exibido vazio, para que eu possa preencher', function () {
+    expect(page.findByName(Login.usuario)).to.exist.and.to.be.empty
+})
+
+Given('que o campo de Senha esteja vazio para que eu possa preencher', function () {
+    expect(page.findByName(Login.senha)).to.be.empty
+})
+
+When('eu preencher o campo de senha', function () {
+    page.findByName(page.findByName(Login.senha)).click()
+    page.write('senha')
+    
+})
+
+Then('o campo irá ocultar os valores digitados', function () {
+
+})
+
+After(async () => {
+    page.quit()
+})
